309e73a417af30e0c6486477f1e5a601d021e534
ReshmaKris <reshmakrisrk@gmail.com>
2018-08-19 19:52:18 +0530

    19/08

--- file1.R
+++ file1.R
@@ -2,3 +2,11 @@ mtcars
 git config --global user.email "reshmakrisrk@gmail.com"
 git config --global user.name "ReshmaKris"
 women
+plot(women)
+install.packages('car')
+?women
+?mean
+x = 1:100
+x
+y = seq(1,100, by=2)
+y
